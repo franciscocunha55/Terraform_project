8feb7be1af2d2d04350e24977b7c7e63ea0eb86b
franciscocunha55 <afrancisco97@gmail.com>
2022-05-13 17:28:29 +0100

    Add user_data to use when boot the instance

--- data.tf
+++ data.tf
@@ -1,1 +1,6 @@
 data "aws_availability_zones" "available" {}
+
+data "template_file" "init" {
+  template = file("${path.module}/userdata.sh.tpl")
+}
+
--- instance.tf
+++ instance.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "server1" {
   instance_type = var.instance_type
   subnet_id = aws_subnet.subnet_public[0].id
   key_name = var.key_name
+  user_data = base64encode(data.template_file.init.rendered)
 
   tags = {
     Name = "instance${local.project_name}"
